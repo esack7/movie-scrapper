b35569f736e37a97a50c2e88cdc723a2c0bcc8ed
Isaac Heist <isaacheist@gmail.com>
2020-03-14 22:36:40 -0700

    added posts array to users

---
+++ mergeData.js
@@ -0,0 +1,9 @@
+module.exports = (priorData, data) =>
+    new Promise((resolve, reject) => {
+        console.log('Need to implement "MERGE"!');
+        try {
+            resolve(data);
+        } catch (err) {
+            reject(err);
+        }
+    });
--- scrapeData.js
+++ scrapeData.js
@@ -1,5 +1,6 @@
 const { getTime, parseJSON } = require('date-fns');
 const { makeGetRequest, writeToFile, fileExists, formatFeed, formatPost, readJSONFile } = require('./utils');
+const mergeData = require('./mergeData');
 require('dotenv').config();
 
 const postURL = process.env.POSTURL;
@@ -8,17 +9,14 @@ const filePath = `./postFeedData.json`;
 module.exports = async function(csrfToken, cookieString) {
     let newURL = postURL;
     const postsFeedDataExists = await fileExists(filePath);
-    let postFeedData = {};
-    let userObj = {};
-    let postItemsObj = {};
-    let feedArray = [];
-
-    if (postsFeedDataExists) {
-        postFeedData = readJSONFile(filePath);
-        userObj = postFeedData.users;
-        postItemsObj = postFeedData.postItems;
-        feedArray = postFeedData.feed;
-    }
+    let postFeedData = {
+        users: {},
+        postItems: {},
+        feed: [],
+    };
+    const userObj = {};
+    const postItemsObj = {};
+    const feedArray = [];
 
     process.stdout.write(`Accessing Posts Data`);
     for (let i = 0; i < 10; i++) {
@@ -29,31 +27,52 @@ module.exports = async function(csrfToken, cookieString) {
         const { users } = res;
         const feed = await formatFeed(res.feed, formatPost);
         newURL = res._links.nextPage.href;
-        // feedArray.push(feed);
-        feed.map(posts => {
-            // console.log(posts);
-            posts.map(
-                post =>
-                    // console.log(post);
-                    null
-            );
+
+        users.map(user => {
+            userObj[`${user.id}`] = {
+                name: user.name,
+                contactInviteId: user.contactInviteId,
+                posts: [],
+            };
             return null;
         });
-        users.map(user => {
-            console.log('User: ', user);
-            // userObj[`${user.id}`] = {
-            //     name: user.name,
-            //     contactInviteId: user.contactInviteId,
-            // };
+
+        feed.map(posts => {
+            posts.map(post => {
+                postItemsObj[`${post.postItemId}`] = {
+                    userId: post.userId,
+                    createdAt: post.createdAt,
+                    updatedAt: post.updatedAt,
+                    editedAt: post.editedAt,
+                };
+                feedArray.push({
+                    text: post.text,
+                    cost: post.cost,
+                    postItemId: post.postItemId,
+                });
+                return null;
+            });
             return null;
         });
     }
     const timeScrapped = parseJSON(getTime(new Date()));
     process.stdout.write(` done!\n`);
 
-    const flattenedFeed = [].concat(...[].concat(...feedArray)).sort((a, b) => b.cost - a.cost);
+    Object.keys(postItemsObj).map(ele => {
+        const user = postItemsObj[`${ele}`].userId;
+        userObj[`${user}`].posts.push(ele);
+        return null;
+    });
 
-    postFeedData = { feed: flattenedFeed, postItems: postItemsObj, users: userObj, time: timeScrapped };
+    const sortedFeed = feedArray.sort((a, b) => b.cost - a.cost);
+    postFeedData = { feed: sortedFeed, postItems: postItemsObj, users: userObj, time: timeScrapped };
 
-    await writeToFile(postFeedData, 'postFeedData');
+    if (postsFeedDataExists) {
+        const priorPostFeedData = await readJSONFile(filePath);
+        const mergedPostFeedData = await mergeData(priorPostFeedData, postFeedData);
+
+        await writeToFile(mergedPostFeedData, 'postFeedData');
+    } else {
+        await writeToFile(postFeedData, 'postFeedData');
+    }
 };
